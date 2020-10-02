0abd2b43af4a51c24e8f315594e8dfad4ab8f7ae
rkkksss <48599078+rkkksss@users.noreply.github.com>
2020-10-03 02:16:39 +0300

    Create 1.cpp

---
+++ ввп/lab7/1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double a;
+    cout << "Введите число А: ";
+    cin >> a;
+    const double pi = 3.14;
+    cout << "Значение  A = " << a << " в радианах: " << pi * a / 180;
+}
