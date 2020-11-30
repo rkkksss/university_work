346fd3000758815c9608a78d43fd4e934f170cd6
rkkksss <48599078+rkkksss@users.noreply.github.com>
2020-11-30 19:22:44 +0300

    Create 1.c

---
+++ introduction to algorithms/4-5/1.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+  int main() {
+  int a[] = {3, 5, 2, 1, 6, 4, 7}; 
+  int n = sizeof(a) / sizeof(int); 
+  for (int i = 0; i < n - 1; i++) 
+    for (int j = 0; j < n - 1 - i; j++) 
+    if (a[j] > a[j+1]) { 
+      int tmp = a[j];
+      a[j] = a[j + 1];
+      a[j + 1] = tmp;
+  }
+  for (int i = 0; i < n; i++)
+    printf("%d ", a[i]);
+}
