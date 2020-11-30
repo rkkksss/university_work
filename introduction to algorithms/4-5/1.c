1b128ef699aaa0d2f6137def8f58f769e2af71c8
rkkksss <48599078+rkkksss@users.noreply.github.com>
2020-11-30 19:24:08 +0300

    Update 1.c

--- introduction to algorithms/4-5/1.c
+++ introduction to algorithms/4-5/1.c
@@ -4,10 +4,10 @@
   int n = sizeof(a) / sizeof(int); 
   for (int i = 0; i < n - 1; i++) 
     for (int j = 0; j < n - 1 - i; j++) 
-    if (a[j] > a[j+1]) { 
-      int tmp = a[j];
-      a[j] = a[j + 1];
-      a[j + 1] = tmp;
+      if (a[j] > a[j+1]) { 
+        int tmp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = tmp;
   }
   for (int i = 0; i < n; i++)
     printf("%d ", a[i]);
