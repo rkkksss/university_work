35e644200a84999db2862a7561f23710a5ade290
rkkksss <48599078+rkkksss@users.noreply.github.com>
2020-12-30 02:56:11 +0300

    Update 5.cpp

--- ввп/lab12/5.cpp
+++ ввп/lab12/5.cpp
@@ -2,17 +2,37 @@
 using namespace std;
 
 int main() {
-  int year, n;
 	setlocale(LC_ALL, "Russian");
-	string variant1[] = {"зелёного", "красного", "жёлтого", "белого", "черного"};
-	string variant2[] = {"зелёной", "красной", "жёлтой", "белой", "черной"};
-	string animal[] = {"крысы", "коровы", "тигра", "зайца", "дракона", "змеи", "лошади", "овцы", "обезьяны", "курицы", "собаки", "свиньи"};
-	cout << "Введите год: ";
-	cin >> year;
-	cout << year << " год - ";
-	year += 56;
-	n = (year % 60) / 12;
-	year = year % 12;
-	if (year == 2 || year == 3 || year == 4) cout << " год " <<  variant1[n] << " " << animal[year] << '.';
-	else cout << " год " <<  variant2[n] << " " << animal[year] << ".";
+	int a;
+	cout << "Введите год\n";
+	cin >> a;
+	if (a > 0) {
+		cout << "Год ";
+		switch (a % 10) {
+			case 0: case 1: cout << "бел"; break;
+			case 2: case 3: cout << "чёрн"; break;
+			case 4: case 5: cout << "зелён"; break;
+			case 6: case 7: cout << "красн"; break;
+			case 8: case 9: cout << "жёлт"; break;
+		}
+		switch (a % 12) {
+			case 0: case 1: case 2: case 3: case 4: case 5: case 9: case 10: case 11: cout << "ой "; break;
+			case 6: case 7: case 8: cout << "ого "; break;
+		}
+		switch ((a + 8) % 12) {
+			case 0: cout << "крысы"; break;
+			case 1: cout << "коровы"; break;
+			case 2: cout << "тигра"; break;
+			case 3: cout << "зайца"; break;
+			case 4: cout << "дракона"; break;
+			case 5: cout << "змеи"; break;
+			case 6: cout << "лошади"; break;
+			case 7: cout << "овцы"; break;
+			case 8: cout << "обезьяны"; break;
+			case 9: cout << "курицы"; break;
+			case 10: cout << "собаки"; break;
+			case 11: cout << "свиньи"; break;
+		}
+	}
+	return 0;
 }
